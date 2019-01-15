49b62668722c0f1d932c950debce8abf59a93d4d
yanagiya <extendcrisis@gmail.com>
2019-01-15 14:45:02 +0900

    mysqlからツイート文を取得

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var Twitter = require('twitter');
 var CronJob = require('cron').CronJob;
+var mysql = require('mysql');
 
 var app = express();
 
+{/*}
 var twitter = new Twitter({
   consumer_key: process.env[`CONSUMER_KEY`],
   consumer_secret: process.env['CONSUMER_SECRET'],
   access_token_key: process.env['ACCESS_TOKEN_KEY'],
   access_token_secret: process.env['ACCESS_TOKEN_SECRET'],
+})
+*/}
+var twitter = new Twitter({
+  consumer_key: `iVEU35YVqtisldohqQil3AdDM`,
+  consumer_secret: 'Na7wYGehx6MPZOIdQUXUCG7IPpXFDczYtUMd1yIBDZ7PGsICTq',
+  access_token_key: '1079396527875928064-Y1Ie4IzhM2FXex0O0WnhKpig7yO6ly',
+  access_token_secret: '8c1SVKAz01M5Dz6D4iMpIFkWUWIOnToVwUWHihbNTtWwE',
 })
 
 var cronTime = '0 * * * * *';
@@ -20,8 +29,23 @@ new CronJob({
   start: true
 })
 
-var tipsArray = ["おはよう","こんにちは","こんばんは","おやすみ","また明日","今日も素晴らしい一日だった","明日はもっと素敵な一日になるだろう"];
-
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'test_user',
+  password : 'password',
+  database : 'test'
+})
+{/*}
+var tipsArray = [
+    "おはよう",
+    "こんにちは",
+    "こんばんは",
+    "おやすみ",
+    "また明日",
+    "今日も素晴らしい一日だった",
+    "明日はもっと素敵な一日になるだろう"
+  ];
+*/}
 function cycleTweet() {
   var tips = tipsArray[Math.floor(Math.random() * tipsArray.length)];
 
